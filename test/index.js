13020ca83031c04a123d198b28313e3eed863adf
Masaaki Morishita <ikasama3.0@gmail.com>
2014-07-26 16:14:14 +0900

    write test code

--- test/index.js
+++ test/index.js
@@ -1,6 +1,35 @@
-var test = require('colored-tape')
-var rework-extend-validator = require('..')
+var fs = require('fs');
+var rework = require('rework');
+var extend = require('rework-inherit')();
+var validator = require('..');
 
-test('description', function (t) {
-  t.end()
-})
+var expect = require('chai').expect;
+
+
+function fixture (name) {
+    return fs.readFileSync('test/fixtures/' + name + '.css', 'utf-8').trim();
+}
+
+
+function compareFixtures (name) {
+    return expect(
+        rework(fixture(name))
+        .use(extend)
+        .use(validator)
+        .toString().trim()
+    ).to.equal(fixture(name + '.out'));
+}
+
+
+function output (name) {
+    return rework(fixture(name))
+           .use(extend)
+           .use(validator)
+           .toString();
+}
+
+describe('test-1', function () {
+    var result = output('test-1');
+
+    expect(result).to.Throw(Error, 'rework-extend-validator: extended rules have same properties');
+});
